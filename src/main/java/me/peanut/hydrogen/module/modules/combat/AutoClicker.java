0dacb22bb03a5ee63e7385f2f03d63d155c1aac4
Zikae <joemancool909@gmail.com>
2025-08-17 16:35:47 -0500

    Update AutoClicker.java

--- src/main/java/me/peanut/hydrogen/module/modules/combat/AutoClicker.java
+++ src/main/java/me/peanut/hydrogen/module/modules/combat/AutoClicker.java
@@ -14,45 +14,54 @@ import java.util.ArrayList;
 import java.util.Random;
 
 /**
- * Created by peanut on 05/02/2021
+ * An improved AutoClicker module.
  */
 
 @Info(name = "AutoClicker", description = "Automatically clicks for you", category = Category.Combat)
 public class AutoClicker extends Module {
 
-    long leftLastSwing;
-    long leftDelay;
-    long rightLastSwing;
-    long rightDelay;
+    private long lastClickTime = 0;
+    private long nextClickDelay = 0;
+    private final Random random = new Random();
 
     public AutoClicker() {
         ArrayList<String> mode = new ArrayList<>();
         mode.add("Left Click");
         mode.add("Right Click");
         addSetting(new Setting("Type", this, "Left Click", mode));
-        addSetting(new Setting("Min. CPS", this, 4, 1, 20, true));
-        addSetting(new Setting("Max. CPS", this, 8, 1, 20, true));
+        addSetting(new Setting("Min. CPS", this, 8, 1, 20, true));
+        addSetting(new Setting("Max. CPS", this, 12, 1, 20, true));
+        addSetting(new Setting("Jitter", this, false));
     }
-
 
     @EventTarget
     public void onUpdate(EventUpdate e) {
         int minCPS = (int) h2.settingsManager.getSettingByName(this, "Min. CPS").getValue();
-        int maxCPS = (int) h2.settingsManager.getSettingByName(this, "Min. CPS").getValue();
+        int maxCPS = (int) h2.settingsManager.getSettingByName(this, "Max. CPS").getValue();
         boolean leftClick = h2.settingsManager.getSettingByName(this, "Type").getMode().equalsIgnoreCase("Left Click");
         boolean rightClick = h2.settingsManager.getSettingByName(this, "Type").getMode().equalsIgnoreCase("Right Click");
+        boolean jitter = h2.settingsManager.getSettingByName(this, "Jitter").getValue();
 
-        if (System.currentTimeMillis() - this.leftLastSwing >= this.leftDelay) {
-            if (mc.gameSettings.keyBindAttack.isKeyDown() && leftClick && System.currentTimeMillis() - this.leftLastSwing >= this.leftDelay) {
-                mc.clickMouse();
-                this.leftLastSwing = System.currentTimeMillis();
-                this.leftDelay = TimeUtils.randomDelay(minCPS, maxCPS);
+        if (System.currentTimeMillis() - lastClickTime >= nextClickDelay) {
+            if (leftClick && mc.gameSettings.keyBindAttack.isKeyDown()) {
+                if (mc.objectMouseOver.entityHit != null) {
+                    mc.clickMouse();
+                    lastClickTime = System.currentTimeMillis();
+                    nextClickDelay = calculateNextDelay(minCPS, maxCPS);
+                    if (jitter) {
+                        doJitter();
+                    }
+                }
             }
-            if (mc.gameSettings.keyBindUseItem.isKeyDown() && !mc.thePlayer.isUsingItem() && rightClick && System.currentTimeMillis() - this.rightLastSwing >= this.rightDelay) {
+            if (rightClick && mc.gameSettings.keyBindUseItem.isKeyDown() && !mc.thePlayer.isUsingItem()) {
                 mc.rightClickMouse();
-                this.rightLastSwing = System.currentTimeMillis();
-                this.rightDelay = TimeUtils.randomDelay(minCPS, maxCPS);
+                lastClickTime = System.currentTimeMillis();
+                nextClickDelay = calculateNextDelay(minCPS, maxCPS);
+                if (jitter) {
+                    doJitter();
+                }
             }
         }
     }
-}
+
+    private long
